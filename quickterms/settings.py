92ae499eff967b6fccb90edb4dc1189a05ec612d
de-marauder <ezikegodson@gmail.com>
2022-07-30 23:16:19 +0100

    fixed the STATICFILES_DIRS error

--- quickterms/settings.py
+++ quickterms/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'tc_site', 'static'),
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
