570aedbec07e67e633105d56355fe21fa1e8df8e
Obianuju007 <onobes007@gmail.com>
2022-08-01 14:45:12 +0100

    Authenticating views

--- tc_site/forms/signupForm.py
+++ tc_site/forms/signupForm.py
@@ -1,1 +1,17 @@
 # Create signup form model here
+from django import forms
+from django.contrib.auth.models import User
+
+
+
+class SignupForm(forms.ModelForm):
+    password2 = forms.CharField(widget = forms.PasswordInput(attrs ={'class':'form-control', 'placeholder':'','required':True}),label='Confirm Password', max_length=20)
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email', 'password', 'password2']
+        widgets = {'first_name': forms.TextInput(attrs={'class':'form-control','placeholder':'', 'required':True}),
+        'username':forms.TextInput(attrs={'class':'form-control','placeholder':''}),
+            'last_name': forms.TextInput(attrs={'class':'form-control','placeholder':'', 'required':True}),
+            'email': forms.EmailInput(attrs={'class':'form-control','placeholder':'', 'required':True}), 
+            'password': forms.PasswordInput(attrs={'class':'form-control','placeholder':'', 'required':True})
+        }
--- tc_site/view_helpers/signup_helper.py
+++ tc_site/view_helpers/signup_helper.py
@@ -4,10 +4,40 @@
 # Import Model from the models folder
 
 # Import Forms from the forms folder
-
 from django.shortcuts import render, redirect
-# Checkout other relevant imports
+from django.contrib.auth.models import User
+from django.contrib import messages
+from tc_site.forms.signupForm import SignupForm
 
+
+# Checkout other relevant imports
 def signup_helper(request):
-    # Write your logic here
-    return # Make sure to return a valid response
+    # Logic here
+    form = SignupForm()
+    if request.method == 'POST':
+        form =  SignupForm(request.POST)
+        if form.is_valid():
+            password = form.cleaned_data['password']
+            password2 = form.cleaned_data['password2']
+            email = form.cleaned_data['email']
+            username = form.cleaned_data['username']
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            # Verifying that user inputs the same password
+            if password!= password2:
+                messages.error(request, "Passwords don't match")
+                return redirect('signup')
+            # Verifying that the email isn't already in the database
+            elif User.objects.filter(email=email).first():
+                messages.error(request, 'Email already exists, try signing in!')
+                return redirect('signup')
+            else:
+                # creating an account for user
+                user = User.objects.create_user(username=username, first_name= first_name, last_name=last_name, email=email, password = password) 
+                # obtaining the username and saving it in the database.
+                user = User.objects.get(username = username)
+                user.save()
+                messages.success(request, 'Account successfully created!')
+                return redirect('signin')
+        return render(request, 'tc_site/pages/signup.html', {'form':form})
+    return render(request, 'tc_site/pages/signup.html', {'form':form})
