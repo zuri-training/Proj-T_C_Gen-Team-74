e669bf45798b2fe06c62223e9d227228ec63c776
de-marauder <ezikegodson@gmail.com>
2022-08-01 12:45:39 +0100

    linked the authentication modals and added a backdrop to the modal

--- tc_site/static/js/modals.js
+++ tc_site/static/js/modals.js
@@ -1,13 +1,17 @@
-const togglePassword = document.querySelector('#togglePassword');
-  const password = document.querySelector('#password');
+const togglePasswordBtn = document.querySelector('#togglePassword');
+const passwords = document.querySelectorAll('.password');
 
-  togglePassword.addEventListener('click', function () {
+passwords.forEach((password) => {
+
+  togglePasswordBtn.addEventListener('click', function () {
     // toggle the type attribute
     const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
     password.setAttribute('type', type);
     // toggle the eye slash icon
     this.classList.toggle('bi-eye');
-});
+  })
+
+})
 
 function enableBtn() {
   var checkBox = document.getElementById('terms_agreement');
@@ -15,7 +19,71 @@ function enableBtn() {
 
   if (checkBox.checked == true) {
     buTn.removeAttribute('disabled');
-} else {
-  buTn.setAttribute('disabled', '');
+  } else {
+    buTn.setAttribute('disabled', '');
+  }
 }
-}
+
+
+let signupModalActive = false;
+let signinModalActive = false;
+
+// Toggle signup modal
+const signupBtns = document.querySelectorAll('.sign-up')
+const signupModal = document.getElementById('sign-up-modal');
+signupModal.style.display = 'none';
+
+
+signupBtns.forEach((signupBtn) => {
+  signupBtn.addEventListener('click', () => {
+    if (signupModalActive) {
+      signupModal.style.display = 'none';
+      signupModalActive = !signupModalActive
+    } else {
+      // remove signin modal if active
+      if (signinModalActive) {
+        signinModal.style.display = 'none';
+        signinModalActive = !signinModalActive
+      }
+      signupModal.style.display = 'grid';
+      signupModalActive = !signupModalActive
+    }
+  })
+})
+
+// Toggle signin modal
+const signinBtns = document.querySelectorAll('.sign-in');
+const signinModal = document.getElementById('sign-in-modal');
+signinModal.style.display = 'none';
+
+signinBtns.forEach((signinBtn) => {
+
+  signinBtn.addEventListener('click', () => {
+    if (signinModalActive) {
+      signinModal.style.display = 'none';
+      signinModalActive = !signinModalActive
+    } else {
+      // remove signup modal if active
+      if (signupModalActive) {
+        signupModal.style.display = 'none';
+        signupModalActive = !signupModalActive
+      }
+      signinModal.style.display = 'grid';
+      signinModalActive = !signinModalActive
+    }
+  })
+
+})
+
+// Add onclick handler to backdrop to remove modal
+const backdrops = document.querySelectorAll('.backdrop')
+backdrops.forEach((backdrop) => {
+  backdrop.addEventListener('click', () => {
+    backdrop.parentElement.style.display = 'none';
+    signinModalActive = false
+    signupModalActive = false
+  })
+})
+
+// switch from signin to signup and vice versa
+
