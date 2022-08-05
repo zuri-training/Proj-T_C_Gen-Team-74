6221164defcd0d9351cb291450691ffc5213b40f
Obianuju007 <onobes007@gmail.com>
2022-08-05 09:43:16 +0100

    forgotten password link fully functional

--- tc_site/urls.py
+++ tc_site/urls.py
@@ -3,6 +3,11 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+# from django.conf.
+
 #!
 #!
 #! If you have to update this file please inform de-marauder first
@@ -20,4 +25,14 @@ urlpatterns = [
     path('download/<int:userID>/<int:docID>', views.download, name="download"),
     path('share/<int:userID>/<int:docID>', views.share, name="share"),
     path('export/<int:userID>/<int:docID>', views.export, name="export"),
+
+    # Reset Password URLS
+    # path('password_reset/', auth_views.PasswordResetView.as_view(template_name='tc_site/pages/password_reset.html'), name='password_reset'),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+    #         template_name='tc_site/pages/password_reset_done.html'),
+    #         name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view              (template_name='tc_site/pages/password_reset_confirm.html'),
+    # name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='tc_site/pages/password_reset_complete.html'),
+    # name='password_reset_complete'),
 ]
