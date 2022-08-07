748d9b7f9f7ef296bc7ebce14b23a122e49596f6
Victor Ihetu <v.ihetu@abbeymortgagebank.com>
2022-08-08 00:09:06 +0100

    changed the background image on a section of the landing page and styled it in line with the new design

--- tc_site/view_helpers/export_helper.py
+++ tc_site/view_helpers/export_helper.py
@@ -15,7 +15,7 @@ from io import BytesIO
 # from django.http import HttpResponse
 from django.template.loader import get_template
 
-from xhtml2pdf import pisa
+# from xhtml2pdf import pisa
 
 def render_to_pdf(template_src, context_dict={}):
     template = get_template(template_src)
