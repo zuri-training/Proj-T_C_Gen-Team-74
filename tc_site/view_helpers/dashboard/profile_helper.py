0a2a8a1cb1b7eab2a7d48511063e29771da16129
de-marauder <ezikegodson@gmail.com>
2022-08-11 01:53:50 +0100

    connected all dashboard routes

--- tc_site/view_helpers/dashboard/profile_helper.py
+++ tc_site/view_helpers/dashboard/profile_helper.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
-def profile_helper (request):
+def profile_helper (request, username):
     user = request.user
 
+    ctx = {
+        'user': user,
+    }
+
+    if username != user.username:
+        print(username)
+        print(user.username)
+
+        messages.error(request, "You are not allowed to view this page")
+        return redirect('/')
+
     if user.id:
-        return render(request, 'tc_site/blocks/dashboard/profile.html')
+        return render(request, 'tc_site/blocks/dashboard/profile.html', ctx)
     
     return redirect('/')
