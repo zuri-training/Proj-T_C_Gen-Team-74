4d1715268ec006dd98d2247890510b2ae03a1565
de-marauder <ezikegodson@gmail.com>
2023-02-02 15:46:20 +0100

    deploy to render

--- quickterms/settings.py
+++ quickterms/settings.py
@@ -156,7 +156,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'tc_site', 'static')
 #     # STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 #     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
